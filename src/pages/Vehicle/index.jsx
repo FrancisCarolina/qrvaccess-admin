acdbce7d93ea889a83e92ff1f79ab87e08692ad6
Francis Carolina <francis.carolinalovo@gmail.com>
2025-01-21 15:19:48 -0300

    buscar veiculo por placa ou nome do condutor

--- src/pages/Vehicle/index.jsx
+++ src/pages/Vehicle/index.jsx
@@ -8,34 +8,51 @@ import './styles.css';
 const VehiclesPage = () => {
     const [vehicles, setVehicles] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState(""); // Estado para o campo de busca
 
     const user = useSelector((state) => state.user.user);
 
-    useEffect(() => {
-        const fetchVehicles = async () => {
-            const token = localStorage.getItem('authToken');
+    const fetchVehicles = async (query = "") => {
+        const token = localStorage.getItem('authToken');
+        if (token && user && user.Local && user.Local.id) {
+            try {
+                const url = query
+                    ? `${process.env.REACT_APP_API_URL}/busca/veiculo/local/${user.Local.id}`
+                    : `${process.env.REACT_APP_API_URL}/veiculo/local/${user.Local.id}`;
 
-            if (token && user && user.Local && user.Local.id) {
-                try {
-                    const response = await axios.get(`${process.env.REACT_APP_API_URL}/veiculo/local/${user.Local.id}`, {
-                        headers: { 'x-access-token': token },
-                    });
-                    console.log("RESPONSE: ", response.data);
+                const params = query ? { busca: query } : null;
 
-                    setVehicles(response.data);
-                    setLoading(false);
-                } catch (error) {
-                    console.error('Erro ao buscar veículos:', error);
-                    setLoading(false);
-                }
-            } else {
-                console.error('Usuário ou token não encontrado.');
+                const response = await axios.get(url, {
+                    headers: { 'x-access-token': token },
+                    params, // Passa os parâmetros apenas se houver busca
+                });
+                setVehicles(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar veículos:', error);
+                setVehicles([]); // Reseta a lista caso haja erro
+            } finally {
                 setLoading(false);
             }
-        };
+        } else {
+            console.error('Usuário ou token não encontrado.');
+            setLoading(false);
+        }
+    };
 
+    // Busca inicial
+    useEffect(() => {
         fetchVehicles();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [user]);
+
+    // Manipulador para o campo de busca
+    const handleSearchChange = (e) => {
+        setLoading(true);
+        const query = e.target.value;
+        setSearch(query);
+        fetchVehicles(query);
+        setLoading(false); // Atualiza a busca com o texto digitado
+    };
 
     if (loading) {
         return <div>Carregando...</div>;
@@ -44,6 +61,13 @@ const VehiclesPage = () => {
     return (
         <Layout>
             <h2 className='mb-4'>Gerenciamento de Veículos</h2>
+            <input
+                type="text"
+                className="search-input"
+                placeholder="Busque por placa ou nome do condutor..."
+                value={search}
+                onChange={handleSearchChange}
+            />
             {vehicles.length === 0 ? (
                 <div className="no-vehicles-message">Nenhum veículo encontrado.</div>
             ) : (
