a85f76b34fab15e8960181d688dc7af891183532
Francis Carolina <francis.carolinalovo@gmail.com>
2025-01-27 14:41:59 -0300

    estilo do perfil

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { setUser } from '../../redux/userSlice';
 import Layout from '../../components/Layout';
+import './styles.css';
+import { FaEnvelope, FaLock, FaMapMarkerAlt } from 'react-icons/fa';
 
 const Perfil = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user.user);
-    const [isEditing, setIsEditing] = useState(false);
+    const [editingField, setEditingField] = useState(null);
     const [formData, setFormData] = useState({
         login: '',
         role_id: '',
@@ -19,9 +21,8 @@ const Perfil = () => {
             nome: ''
         }
     });
-
-    const [passwordError, setPasswordError] = useState('');
     const navigate = useNavigate();
+    const cardEditRef = useRef(null);
 
     useEffect(() => {
         if (!user) {
@@ -39,10 +40,6 @@ const Perfil = () => {
             });
         }
     }, [user, navigate]);
-
-    const handleEditProfile = () => {
-        setIsEditing(true);
-    };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -64,11 +61,6 @@ const Perfil = () => {
     };
 
     const handleSaveChanges = async () => {
-        if (formData.senha !== formData.confirmPassword) {
-            setPasswordError('As senhas não coincidem');
-            return;
-        }
-
         const updatedUser = {
             login: formData.login,
             senha: formData.senha,
@@ -82,9 +74,7 @@ const Perfil = () => {
             });
 
             dispatch(setUser({ user: response.data }));
-
-            setIsEditing(false);
-            setPasswordError('');
+            setEditingField(null);
             alert('Perfil atualizado com sucesso!');
         } catch (error) {
             console.error('Erro ao atualizar o perfil:', error);
@@ -92,80 +82,117 @@ const Perfil = () => {
         }
     };
 
-    const handleCancelEdit = () => {
-        // Cancela as edições, restaurando os dados do usuário
-        setIsEditing(false);
-        setFormData({
-            login: user.login,
-            role_id: user.role_id,
-            senha: '',
-            confirmPassword: '',
-            local: {
-                id: user.Local?.id || '',
-                nome: user.Local?.nome || ''
+
+
+
+
+    useEffect(() => {
+        const handleCancelEdit = () => {
+            setEditingField(null);
+            setFormData({
+                login: user.login,
+                role_id: user.role_id,
+                senha: '',
+                confirmPassword: '',
+                local: {
+                    id: user.Local?.id || '',
+                    nome: user.Local?.nome || ''
+                }
+            });
+        };
+        const handleClickOutside = (event) => {
+            if (cardEditRef.current && !cardEditRef.current.contains(event.target)) {
+                handleCancelEdit();
             }
-        });
-        setPasswordError('');
-    };
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [user]);
 
     return (
         <Layout>
-            <h2>Perfil de {user?.login}</h2>
-            <div className="perfil-info">
-                {isEditing ? (
-                    <>
-                        <div className="form-group">
-                            <label>Login</label>
-                            <input
-                                type="text"
-                                name="login"
-                                value={formData.login}
-                                onChange={handleChange}
-                                className="form-control"
-                            />
+            <div className="container-perfil">
+                <h2>Perfil de {user?.login}</h2>
+                <section>
+                    <div>Login:</div>
+                    <div>{user?.login}</div>
+                </section>
+                <section>
+                    <div>Função:</div>
+                    <div>{user?.role_id === 1 ? "Administrador" : "Condutor"}</div>
+                </section>
+                <section>
+                    <div>Local:</div>
+                    <div>{user?.Local?.nome}</div>
+                </section>
+                <div className="card-perfil">
+                    {editingField === 'login' ? (
+                        <div className="card-edit" ref={cardEditRef}>
+                            <label htmlFor="login">Novo login:</label>
+                            <div className="edit-form">
+                                <input
+                                    id="login"
+                                    type="text"
+                                    name="login"
+                                    value={formData.login}
+                                    onChange={handleChange}
+                                    placeholder="Digite o novo login"
+                                />
+                                <button onClick={handleSaveChanges}>Salvar</button>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label>Senha</label>
-                            <input
-                                type="password"
-                                name="senha"
-                                value={formData.senha}
-                                onChange={handleChange}
-                                className="form-control"
-                            />
+                    ) : (
+                        <div className="card" onClick={() => setEditingField('login')}>
+                            <FaEnvelope />
+                            <div>Atualizar Login</div>
                         </div>
-                        <div className="form-group">
-                            <label>Confirmar Senha</label>
-                            <input
-                                type="password"
-                                name="confirmPassword"
-                                value={formData.confirmPassword}
-                                onChange={handleChange}
-                                className="form-control"
-                            />
+                    )}
+                    {editingField === 'senha' ? (
+                        <div className="card-edit" ref={cardEditRef}>
+                            <label htmlFor="senha">Nova senha:</label>
+                            <div className="edit-form">
+                                <input
+                                    id="senha"
+                                    type="password"
+                                    name="senha"
+                                    value={formData.senha}
+                                    onChange={handleChange}
+                                    placeholder="Digite a nova senha"
+                                />
+                                <button onClick={handleSaveChanges}>Salvar</button>
+                            </div>
                         </div>
-                        {passwordError && <div className="text-danger">{passwordError}</div>}
-                        <div className="form-group">
-                            <label>Local</label>
-                            <input
-                                type="text"
-                                name="local.nome"
-                                value={formData.local.nome}
-                                onChange={handleChange}
-                                className="form-control"
-                            />
+                    ) : (
+                        <div className="card" onClick={() => setEditingField('senha')}>
+                            <FaLock />
+                            <div>Alterar Senha</div>
                         </div>
-                        <button className="btn btn-primary mt-3" style={{ marginRight: "20px" }} onClick={handleSaveChanges}>Salvar alterações</button>
-                        <button className="btn btn-secondary mt-3" onClick={handleCancelEdit}>Cancelar</button>
-                    </>
-                ) : (
-                    <>
-                        <p><strong>Login:</strong> {user?.login}</p>
-                        <p><strong>Função:</strong> {user?.role_id}</p>
-                        <p><strong>Local:</strong> {user?.Local?.nome}</p>
-                        <button className="btn btn-warning" onClick={handleEditProfile}>Editar Login</button>
-                    </>
-                )}
+                    )}
+                    {editingField === 'local' ? (
+                        <div className="card-edit" ref={cardEditRef}>
+                            <label htmlFor="local">Novo nome do local:</label>
+                            <div className="edit-form">
+                                <input
+                                    id="local"
+                                    type="text"
+                                    name="local.nome"
+                                    value={formData.local.nome}
+                                    onChange={handleChange}
+                                    placeholder="Digite o novo nome do local"
+                                />
+                                <button onClick={handleSaveChanges}>Salvar</button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="card" onClick={() => setEditingField('local')}>
+                            <FaMapMarkerAlt />
+                            <div>Alterar Nome do Local</div>
+                        </div>
+                    )}
+                </div>
+                <button className="logout-button">Logout</button>
             </div>
         </Layout>
     );
