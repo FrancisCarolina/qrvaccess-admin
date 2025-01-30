2e6047359fe1acd2c7b2314372790ed54b81db7c
Francis Carolina <francis.carolinalovo@gmail.com>
2025-01-30 14:31:46 -0300

    colocando mascara de cpf

--- src/components/Card/DriverCard/index.jsx
+++ src/components/Card/DriverCard/index.jsx
@@ -3,6 +3,10 @@ import './styles.css';
 
 const DriverCard = ({ driver, onEdit }) => {
     const status = driver.ativo ? 'Ativo' : 'Inativo';
+    const formatCPF = (cpf) => {
+        if (!cpf) return '';
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    };
     return (
         <div className="card-condutores" onClick={() => onEdit(driver.Usuario?.id)}>
             <div className="card-titulo">
@@ -12,7 +16,7 @@ const DriverCard = ({ driver, onEdit }) => {
             <div className="condutor-content">
                 <div>
                     <p>CPF:</p>
-                    <p>{driver.cpf}</p>
+                    <p>{formatCPF(driver.cpf)}</p>
                 </div>
                 <div>
                     <p>Login:</p>
